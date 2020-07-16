4b64c90b74b7a161f42adf462b09810fad6d331e
lerderl <josepholukunle1107@gmail.com>
2020-07-16 10:45:21 +0100

    Handling errors locally

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import classes from './Blog.module.css';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount() {
@@ -23,6 +24,10 @@ class Blog extends Component {
             
             this.setState({posts: updatedPosts});
             // console.log(response);
+        })
+        .catch(error => {
+            // console.log(error);
+            this.setState({error: true});
         });
     }
 
@@ -31,10 +36,13 @@ class Blog extends Component {
     }
     
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author} 
-                         clicked={() => this.postSelectedHandler(post.id)} />
-        });
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post key={post.id} title={post.title} author={post.author} 
+                             clicked={() => this.postSelectedHandler(post.id)} />
+            });
+        }
         
         return (
             <div>
