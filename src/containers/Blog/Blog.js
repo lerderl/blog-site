b15fc04443467d7cdc6609b52676c5434cb08209
lerderl <josepholukunle1107@gmail.com>
2020-07-16 11:26:57 +0100

    Preparing the project for routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,47 +1,9 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
-import axios from '../../axios';
 import classes from './Blog.module.css';
+import Posts from './Posts/Posts';
 
-class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
-
-    componentDidMount() {
-        axios.get('/posts').then(response => {
-            const posts = response.data.slice(0, 4);
-            const updatedPosts = posts.map(post => {
-                return {
-                    ...post,
-                    author: 'God'
-                }
-            });
-            
-            this.setState({posts: updatedPosts});
-            // console.log(response);
-        })
-        .catch(error => {
-            // console.log(error);
-            this.setState({error: true});
-        });
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
-    }
-    
-    render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post key={post.id} title={post.title} author={post.author} 
-                             clicked={() => this.postSelectedHandler(post.id)} />
-            });
-        }
-        
+class Blog extends Component {    
+    render () {        
         return (
             <div className={classes.Blog}>
                 <header>
@@ -52,9 +14,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className={classes.Posts}>
-                    {posts}
-                </section>
+                <Posts />                
             </div>
         );
     }
