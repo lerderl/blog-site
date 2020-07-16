5b0d7564e9e16c9d7869eacab8f716a92b3dd9da
lerderl <josepholukunle1107@gmail.com>
2020-07-16 11:05:55 +0100

    Using interceptors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(request => {
+  console.log(request);
+  // You can edit the request config
+  return request;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+  console.log(response);
+  // You can edit the response config
+  return response;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
 
 ReactDOM.render(
   <React.StrictMode>
